7147a36dcd064877452720bbd4fac210063c7531
Adam Ryšánek <adamrysanek@Adam-MacBook-Pro.local>
2022-10-12 20:07:09 +0200

    Add Layout component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  Home
+  <Layout>
+    Home
+  </Layout>
 </template>
 
 <script>
+import Layout from './components/Layout.vue'
+import 'reset-css';
+
 export default {
   name: 'App',
+  components: { Layout }
 }
 </script>
---
+++ src/components/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="bg-light px-2 min-vh-100 h-100">
+		<div class="container h-100">
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'PageLayout',
+}
+</script>
