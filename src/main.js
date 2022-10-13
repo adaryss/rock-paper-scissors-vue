fa6cbcb1fbf5927d000144d9684ab940e3b1b290
Adam Ryšánek <adamrysanek@Adam-MacBook-Pro.local>
2022-10-13 17:42:09 +0200

    Add boostrap-icons package

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App).mount('#app')
